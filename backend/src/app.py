b068f36a69d81fdf3f6bacc6ba7fcb529ce7e520
margongora <margongora0803@gmail.com>
2024-10-03 14:53:53 -0500

    added bcrypt for hashing passwords

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,44 +1,78 @@
 from dotenv import load_dotenv
-from flask import Flask, request, render_template
+from flask import flash, Flask, request, render_template, redirect, url_for
+from flask_bcrypt import Bcrypt
 from user import User
 from db import create_user, get_user_by_id, get_user_by_email
 
 import os
 import flask_login
 
-
+# Load env variables
 load_dotenv()
 
+# Create app and login manager
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
+bcrypt = Bcrypt(app)
+
+# Set up way to load users into a session
 @login_manager.user_loader
 def load_user(user_id):
     return get_user_by_id(user_id)
 
-
+# Routes
 @app.route('/')
 def howdy_world():
     return '<h1>Howdy, world!</h1>'
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if flask_login.current_user.is_authenticated:
+        return 'You are already registered.'
 
-@app.route('/register', methods=['POST'])
-def register_user():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    create_user(email, password)
-   
-    return 'REGISTERED!'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
 
+        if get_user_by_email(email) != None:
+            return 'User exists'
 
-@app.route('/login', methods=['POST'])
-def login_user():
-    email = request.form.get('email')
-    password = request.form.get('password')
+        create_user(email, bcrypt.generate_password_hash(password))
+    
+        return 'REGISTERED!'
+    
+    return 'Please register here.'
 
-    if password == get_user_by_email(email):
-        return 'Welcome!'
-    return 'Invalid login!'
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask_login.current_user.is_authenticated:
+        return 'You are already logged in.'
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = get_user_by_email(email)
+
+        if bcrypt.check_password_hash(user.password, password):
+            flask_login.login_user(user)
+            return 'Welcome!'
+        return 'Invalid login!'
+    
+    return 'Please log in here.'  
+
+@app.route('/logout')
+@flask_login.login_required
+def logout():
+    flask_login.logout_user()
+    flash('You are logged out', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+@flask_login.login_required
+def profile():
+    return f'Howdy, {flask_login.current_user.email}'
--- backend/src/db.py
+++ backend/src/db.py
@@ -11,13 +11,13 @@ DB_PASS = os.getenv('DB_PASS')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = int(os.getenv('DB_PORT'))
 
+# Define method to generate a connection to the DB
 def get_connection(): 
     return pymysql.connect(host=DB_HOST,
                            user=DB_USER, 
                            passwd=DB_PASS, 
                            port=DB_PORT, 
                            db=DB_NAME)
-
 
 # Define method to create user given email and password
 def create_user(email: str, password: str):
@@ -28,17 +28,16 @@ def create_user(email: str, password: str):
         
         cnx.commit()
 
-
-# Define method to get user's hashed password from database
+# Define method to get user by email from database
 def get_user_by_email(email: str):
     with get_connection().cursor() as cursor:
-        sql = "SELECT `password` FROM `users` WHERE `email` = %s"
+        sql = "SELECT * FROM `users` WHERE `email` = %s"
         cursor.execute(sql, (email))
         
-        return None if (password := cursor.fetchone()) == None else password[0]
+        return None if (user := cursor.fetchone()) == None else User(user[0], user[1], user[2])
 
 # Define method to get user by id from database
-def get_user_by_id(user_id: str):
+def get_user_by_id(user_id: str) -> User:
     with get_connection().cursor() as cursor:
         sql = "SELECT * FROM `users` WHERE `id` = %(id)s"
         cursor.execute(sql, args={'id': int(user_id)})
